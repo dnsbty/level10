0557102c139af0f625d7a41092f735a39fbde67a
Dennis Beatty <dennis@dcbeatty.com>
2020-03-19 22:21:14 -0600

    set up elixir releases
    This should set everything up to generate releases for deployment.

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :level10, Level10Web.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "level10.games", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
@@ -52,4 +52,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
---
+++ config/releases.exs
@@ -0,0 +1,17 @@
+import Config
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :level10, Level10Web.Endpoint,
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  secret_key_base: secret_key_base
+
+config :level10, Level10Web.Endpoint, server: true
