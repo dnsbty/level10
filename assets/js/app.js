fec18a989a2f853786d32c36d89e668c1aa6e5ea
Dennis Beatty <dennis@dcbeatty.com>
2020-04-06 11:10:44 -0600

    focus name input on display
    This will focus the keyboard on the name input whenever it is shown so
    that users can immediately take the next action without having to think
    about it first.

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,17 @@ import LiveSocket from 'phoenix_live_view';
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute('content');
+
+let Hooks = {};
+
+Hooks.NameInput = {
+  mounted() {
+    this.el.focus();
+  },
+};
+
 let liveSocket = new LiveSocket('/live', Socket, {
+  hooks: Hooks,
   params: {_csrf_token: csrfToken},
 });
 liveSocket.connect();
