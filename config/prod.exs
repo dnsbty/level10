1caa4638a6d49c6e49e3b0381231df20ada0e6b0
Dennis Beatty <dennis@dcbeatty.com>
2020-06-15 17:47:40 -0600

    temporarily enable debug logs

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :level10, Level10Web.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+# config :logger, level: :info
 
 # Configure clustering
 config :level10,
