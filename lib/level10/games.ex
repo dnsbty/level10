c2cb30a0bb853e95d62f51c988e2ab52890fca9e
Dennis Beatty <dennis@dcbeatty.com>
2020-07-27 14:12:24 -0600

    add overridable timeouts to genserver functions

--- lib/level10/games.ex
+++ lib/level10/games.ex
@@ -20,13 +20,20 @@ defmodule Level10.Games do
   @doc """
   Add one or more cards to a group that is already on the table
   """
-  @spec add_to_table(Game.join_code(), Player.id(), Player.id(), non_neg_integer(), Game.cards()) ::
+  @spec add_to_table(
+          Game.join_code(),
+          Player.id(),
+          Player.id(),
+          non_neg_integer(),
+          Game.cards(),
+          timeout()
+        ) ::
           :ok | :invalid_group | :level_incomplete | :needs_to_draw | :not_your_turn
-  def add_to_table(join_code, player_id, table_id, position, cards_to_add) do
+  def add_to_table(join_code, player_id, table_id, position, cards_to_add, timeout \\ 5000) do
     GenServer.call(
       via(join_code),
       {:add_to_table, {player_id, table_id, position, cards_to_add}},
-      5000
+      timeout
     )
   end
 
@@ -51,9 +58,9 @@ defmodule Level10.Games do
   @doc """
   Returns a Player struct representing the player who created the game.
   """
-  @spec creator(Game.join_code()) :: Player.t()
-  def creator(join_code) do
-    GenServer.call(via(join_code), :creator, 5000)
+  @spec creator(Game.join_code(), timeout()) :: Player.t()
+  def creator(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :creator, timeout)
   end
 
   @doc """
@@ -64,9 +71,9 @@ defmodule Level10.Games do
       iex> current_player_has_drawn?("ABCD")
       true
   """
-  @spec current_player_has_drawn?(Game.join_code()) :: boolean()
-  def current_player_has_drawn?(join_code) do
-    GenServer.call(via(join_code), :current_turn_drawn?, 5000)
+  @spec current_player_has_drawn?(Game.join_code(), timeout()) :: boolean()
+  def current_player_has_drawn?(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :current_turn_drawn?, timeout)
   end
 
   @doc """
@@ -90,10 +97,10 @@ defmodule Level10.Games do
       iex> discard_card("ABCD", "9c34b9fe-3104-44b3-b21b-28140e2e3624", %Card{color: :green, value: :twelve})
       :ok
   """
-  @spec discard_card(Game.join_code(), Player.id(), Card.t()) ::
+  @spec discard_card(Game.join_code(), Player.id(), Card.t(), timeout()) ::
           :ok | :needs_to_draw | :not_your_turn
-  def discard_card(join_code, player_id, card) do
-    GenServer.call(via(join_code), {:discard, {player_id, card}}, 5000)
+  def discard_card(join_code, player_id, card, timeout \\ 5000) do
+    GenServer.call(via(join_code), {:discard, {player_id, card}}, timeout)
   end
 
   @doc """
@@ -108,10 +115,10 @@ defmodule Level10.Games do
       iex> draw_card("ABCD", "9c34b9fe-3104-44b3-b21b-28140e2e3624", :discard_pile)
       %Card{color: :green, value: :twelve}
   """
-  @spec draw_card(Game.join_code(), Player.id(), :discard_pile | :draw_pile) ::
+  @spec draw_card(Game.join_code(), Player.id(), :discard_pile | :draw_pile, timeout()) ::
           Card.t() | :already_drawn | :empty_discard_pile | :not_your_turn | :skip
-  def draw_card(join_code, player_id, source) do
-    GenServer.call(via(join_code), {:draw, {player_id, source}}, 5000)
+  def draw_card(join_code, player_id, source, timeout \\ 5000) do
+    GenServer.call(via(join_code), {:draw, {player_id, source}}, timeout)
   end
 
   @doc """
@@ -141,9 +148,9 @@ defmodule Level10.Games do
       iex> finished?("ABCD")
       true
   """
-  @spec finished?(Game.join_code()) :: boolean()
-  def finished?(join_code) do
-    GenServer.call(via(join_code), :finished?, 5000)
+  @spec finished?(Game.join_code(), timeout()) :: boolean()
+  def finished?(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :finished?, timeout)
   end
 
   @doc """
@@ -154,9 +161,9 @@ defmodule Level10.Games do
       iex> get("ABCD")
       %Game{}
   """
-  @spec get(Game.join_code()) :: Game.t()
-  def get(join_code) do
-    GenServer.call(via(join_code), :get, 5000)
+  @spec get(Game.join_code(), timeout()) :: Game.t()
+  def get(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :get, timeout)
   end
 
   @doc """
@@ -167,9 +174,9 @@ defmodule Level10.Games do
       iex> get_current_turn("ABCD")
       %Player{id: "ffe6629a-faff-4053-b7b8-83c3a307400f", name: "Player 1"}
   """
-  @spec get_current_turn(Game.join_code()) :: Player.t()
-  def get_current_turn(join_code) do
-    GenServer.call(via(join_code), :current_player, 5000)
+  @spec get_current_turn(Game.join_code(), timeout()) :: Player.t()
+  def get_current_turn(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :current_player, timeout)
   end
 
   @doc """
@@ -180,9 +187,11 @@ defmodule Level10.Games do
       iex> get_hand_counts("ABCD")
       %{"179539f0-661e-4b56-ac67-fec916214223" => 10, "000cc69a-bb7d-4d3e-ae9f-e42e3dcac23e" => 3}
   """
-  @spec get_hand_counts(Game.join_code()) :: %{optional(Player.id()) => non_neg_integer()}
-  def get_hand_counts(join_code) do
-    GenServer.call(via(join_code), :hand_counts, 5000)
+  @spec get_hand_counts(Game.join_code(), timeout()) :: %{
+          optional(Player.id()) => non_neg_integer()
+        }
+  def get_hand_counts(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :hand_counts, timeout)
   end
 
   @doc """
@@ -193,9 +202,9 @@ defmodule Level10.Games do
       iex> get_hand_for_player("ABCD", "557489d0-1ef2-4763-9b0b-d2ea3c80fd99")
       [%Card{color: :green, value: :twelve}, %Card{color: :blue, value: :nine}, ...]
   """
-  @spec get_hand_for_player(Game.join_code(), Player.id()) :: list(Card.t())
-  def get_hand_for_player(join_code, player_id) do
-    GenServer.call(via(join_code), {:hand, player_id}, 5000)
+  @spec get_hand_for_player(Game.join_code(), Player.id(), timeout()) :: list(Card.t())
+  def get_hand_for_player(join_code, player_id, timeout \\ 5000) do
+    GenServer.call(via(join_code), {:hand, player_id}, timeout)
   end
 
   @doc """
@@ -209,9 +218,9 @@ defmodule Level10.Games do
         "86800484-8e73-4408-bd15-98a57871694f" => [run: 7],
       }
   """
-  @spec get_levels(Game.join_code()) :: %{optional(Player.t()) => Levels.level()}
-  def get_levels(join_code) do
-    levels = GenServer.call(via(join_code), :levels, 5000)
+  @spec get_levels(Game.join_code(), timeout()) :: %{optional(Player.t()) => Levels.level()}
+  def get_levels(join_code, timeout \\ 5000) do
+    levels = GenServer.call(via(join_code), :levels, timeout)
 
     for {player_id, level_number} <- levels,
         into: %{},
@@ -229,25 +238,25 @@ defmodule Level10.Games do
         %Player{id: "a0d2ef3e-e44c-4a58-b90d-a56d88224700", name: "Player 2"}
       ]
   """
-  @spec get_players(Game.join_code()) :: list(Player.t())
-  def get_players(join_code) do
-    GenServer.call(via(join_code), :players, 5000)
+  @spec get_players(Game.join_code(), timeout) :: list(Player.t())
+  def get_players(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :players, timeout)
   end
 
   @doc """
   Gets the set of IDs of players who are ready for the next round to begin.
   """
-  @spec get_players_ready(Game.join_code()) :: MapSet.t(Player.id())
-  def get_players_ready(join_code) do
-    GenServer.call(via(join_code), :players_ready, 5000)
+  @spec get_players_ready(Game.join_code(), timeout()) :: MapSet.t(Player.id())
+  def get_players_ready(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :players_ready, timeout)
   end
 
   @doc """
   Get the round number for the current round.
   """
-  @spec get_round_number(Game.join_code()) :: non_neg_integer()
-  def get_round_number(join_code) do
-    GenServer.call(via(join_code), :current_round, 5000)
+  @spec get_round_number(Game.join_code(), timeout()) :: non_neg_integer()
+  def get_round_number(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :current_round, timeout)
   end
 
   @doc """
@@ -261,9 +270,9 @@ defmodule Level10.Games do
         "38379e46-4d29-4a22-a245-aa7013ec3c33" => {2, 120}
       }
   """
-  @spec get_scores(Game.join_code()) :: Game.scores()
-  def get_scores(join_code) do
-    GenServer.call(via(join_code), :scoring, 5000)
+  @spec get_scores(Game.join_code(), timeout()) :: Game.scores()
+  def get_scores(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :scoring, timeout)
   end
 
   @doc """
@@ -288,9 +297,9 @@ defmodule Level10.Games do
         }
       }
   """
-  @spec get_table(Game.join_code()) :: Game.table()
-  def get_table(join_code) do
-    GenServer.call(via(join_code), :table, 5000)
+  @spec get_table(Game.join_code(), timeout()) :: Game.table()
+  def get_table(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :table, timeout)
   end
 
   @doc """
@@ -304,9 +313,9 @@ defmodule Level10.Games do
       iex> get_top_discarded_card("ABCD")
       nil
   """
-  @spec get_top_discarded_card(Game.join_code()) :: Card.t() | nil
-  def get_top_discarded_card(join_code) do
-    GenServer.call(via(join_code), :top_discarded_card, 5000)
+  @spec get_top_discarded_card(Game.join_code(), timeout()) :: Card.t() | nil
+  def get_top_discarded_card(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :top_discarded_card, timeout)
   end
 
   @doc """
@@ -327,13 +336,13 @@ defmodule Level10.Games do
       iex> join_game("ABCD", "Player One")
       :not_found
   """
-  @spec join_game(Game.join_code(), String.t()) ::
+  @spec join_game(Game.join_code(), String.t(), timeout()) ::
           {:ok, Player.id()} | :already_started | :full | :not_found
-  def join_game(join_code, player_name) do
+  def join_game(join_code, player_name, timeout \\ 5000) do
     player = Player.new(player_name)
 
     if exists?(join_code) do
-      GenServer.call(via(join_code), {:join, player}, 5000)
+      GenServer.call(via(join_code), {:join, player}, timeout)
     else
       :not_found
     end
@@ -346,9 +355,9 @@ defmodule Level10.Games do
   If the player is currently alone in the game, the game will be deleted as
   well.
   """
-  @spec leave_game(Game.join_code(), Player.id()) :: :ok | :already_started | :deleted
-  def leave_game(join_code, player_id) do
-    result = GenServer.call(via(join_code), {:delete_player, player_id}, 5000)
+  @spec leave_game(Game.join_code(), Player.id(), timeout()) :: :ok | :already_started | :deleted
+  def leave_game(join_code, player_id, timeout \\ 5000) do
+    result = GenServer.call(via(join_code), {:delete_player, player_id}, timeout)
     with :empty_game <- result, do: delete_game(join_code)
   end
 
@@ -373,12 +382,14 @@ defmodule Level10.Games do
   @doc """
   Returns whether or not the specified player exists within the specified game.
   """
-  @spec player_exists?(Game.t() | Game.join_code(), Player.id()) :: boolean()
-  def player_exists?(join_code, player_id) when is_binary(join_code) do
-    GenServer.call(via(join_code), {:player_exists?, player_id}, 5000)
+  @spec player_exists?(Game.t() | Game.join_code(), Player.id(), timeout()) :: boolean()
+  def player_exists?(join_code_or_game, player_id, timeout \\ 5000)
+
+  def player_exists?(join_code, player_id, timeout) when is_binary(join_code) do
+    GenServer.call(via(join_code), {:player_exists?, player_id}, timeout)
   end
 
-  def player_exists?(game, player_id), do: Game.player_exists?(game, player_id)
+  def player_exists?(game, player_id, _), do: Game.player_exists?(game, player_id)
 
   @doc """
   Check whether or not the current round has started.
@@ -391,17 +402,17 @@ defmodule Level10.Games do
       iex> round_started?("EFGH")
       false
   """
-  @spec round_started?(Game.join_code()) :: boolean()
-  def round_started?(join_code) do
-    GenServer.call(via(join_code), :round_started?, 5000)
+  @spec round_started?(Game.join_code(), timeout()) :: boolean()
+  def round_started?(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :round_started?, timeout)
   end
 
   @doc """
   Returns the player struct representing the player who won the current round.
   """
-  @spec round_winner(Game.join_code()) :: Player.t() | nil
-  def round_winner(join_code) do
-    GenServer.call(via(join_code), :round_winner, 5000)
+  @spec round_winner(Game.join_code(), timeout()) :: Player.t() | nil
+  def round_winner(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :round_winner, timeout)
   end
 
   @doc """
@@ -423,9 +434,9 @@ defmodule Level10.Games do
       iex> started?("EFGH")
       false
   """
-  @spec started?(Game.join_code()) :: boolean()
-  def started?(join_code) do
-    GenServer.call(via(join_code), :started?, 5000)
+  @spec started?(Game.join_code(), timeout()) :: boolean()
+  def started?(join_code, timeout \\ 5000) do
+    GenServer.call(via(join_code), :started?, timeout)
   end
 
   @doc """
@@ -445,10 +456,10 @@ defmodule Level10.Games do
   @doc """
   Set the given player's table to the given cards.
   """
-  @spec table_cards(Game.join_code(), Player.id(), Game.player_table()) ::
+  @spec table_cards(Game.join_code(), Player.id(), Game.player_table(), timeout()) ::
           :ok | :already_set | :needs_to_draw | :not_your_turn
-  def table_cards(join_code, player_id, player_table) do
-    GenServer.call(via(join_code), {:table_cards, {player_id, player_table}}, 5000)
+  def table_cards(join_code, player_id, player_table, timeout \\ 5000) do
+    GenServer.call(via(join_code), {:table_cards, {player_id, player_table}}, timeout)
   end
 
   @doc """
@@ -468,6 +479,7 @@ defmodule Level10.Games do
   """
   @spec update(Game.join_code(), (Game.t() -> Game.t())) :: :ok
   def update(join_code, fun) do
+    # TODO: Move this to cast
     GenServer.call(via(join_code), {:update, fun}, 5000)
   end
 
