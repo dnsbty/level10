9ae4353f4e3b92d2459d014e36afbc714852b9b2
Dennis Beatty <dennis@dcbeatty.com>
2020-08-20 15:11:30 -0600

    fix security vulnerability
    One of the Javascript dependencies, serialize-javascript, had a severe
    security vulnerability in its code. See CVE-2020-7660
    
    This updates the dependencies that rely on serialize-javascript to get
    it updated to a more secure version.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,26 +1,26 @@
-const path = require('path');
-const glob = require('glob');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, options) => {
-  const devMode = options.mode !== 'production';
+  const devMode = options.mode !== "production";
 
   return {
     optimization: {
       minimizer: [
-        new TerserPlugin({cache: true, parallel: true, sourceMap: devMode}),
-        new OptimizeCSSAssetsPlugin({}),
-      ],
+        new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
     },
     entry: {
-      './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js']),
+      "./js/app.js": glob.sync("./vendor/**/*.js").concat(["./js/app.js"])
     },
     output: {
-      filename: 'app.js',
-      path: path.resolve(__dirname, '../priv/static/js'),
+      filename: "app.js",
+      path: path.resolve(__dirname, "../priv/static/js")
     },
     module: {
       rules: [
@@ -28,18 +28,18 @@ module.exports = (env, options) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
-          },
+            loader: "babel-loader"
+          }
         },
         {
           test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-        },
-      ],
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+        }
+      ]
     },
     plugins: [
-      new MiniCssExtractPlugin({filename: '../css/app.css'}),
-      new CopyWebpackPlugin([{from: 'static/', to: '../'}]),
-    ],
+      new MiniCssExtractPlugin({ filename: "../css/app.css" }),
+      new CopyWebpackPlugin({ patterns: [{ from: "static/", to: "../" }] })
+    ]
   };
 };
