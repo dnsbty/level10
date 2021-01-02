b2b3e462093d53eb07fc0a53675d760d33dfa945
Dennis Beatty <dennis@dcbeatty.com>
2021-01-02 03:02:21 -0700

    fix URLs sent in emails
    The emails sent for authentication events don't work properly because
    they show port 443 in the URL while showing "http" for the URL scheme.
    This fixes that so that the endpoint will expect the scheme to be
    "https" and the port to be 443.

--- config/prod.exs
+++ config/prod.exs
@@ -9,7 +9,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :level10, Level10Web.Endpoint,
-  url: [host: "level10.games", port: 443],
+  url: [host: "level10.games", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
