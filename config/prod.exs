98bcb5616ad63685e2738f9154363e42315a1972
Matt <matthewgilley@outlook.com>
2022-09-16 21:42:05 +0000

    testing

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :level10, Level10Web.Endpoint,
-  url: [host: "localhost", port: 443, scheme: "https"],
+  url: [port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
