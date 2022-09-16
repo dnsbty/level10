c1f8fdef6137ddeaf14c6bc329a8673c25e1e1b6
Matt <matthewgilley@outlook.com>
2022-09-16 21:59:37 +0000

    Revert "testing"
    This reverts commit 16935ad0b9ab2e90c7d205719920003eb3960e1f.

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :level10, Level10Web.Endpoint,
-  http: [port: 4002],
-  server: false
+  url: [port: 443, scheme: "https"],
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
